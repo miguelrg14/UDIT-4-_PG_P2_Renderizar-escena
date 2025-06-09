6ac131a6e03390c10f22cc80a368dd26cb35895f
Miguel Rodríguez Gallego <miguelrodriguezgallego1@gmail.com>
2025-06-09 21:07:49 +0200

    Postprocesado funciona (Prueba sin texturas y escena amarronada)

--- code/Scene.cpp
+++ code/Scene.cpp
@@ -81,6 +81,37 @@ namespace udit
         "    fragment_color = vec4(front_color, 0.5) * texture_color;\n" // Resultado final visual
         "}";
 
+    const string Scene::effect_vertex_shader_code =
+
+        "#version 330\n"
+        ""
+        "layout (location = 0) in vec3 vertex_coordinates;"
+        "layout (location = 1) in vec2 vertex_texture_uv;"
+        ""
+        "out vec2 texture_uv;"
+        ""
+        "void main()"
+        "{"
+        "   gl_Position = vec4(vertex_coordinates, 1.0);"
+        "   texture_uv  = vertex_texture_uv;"
+        "}";
+
+    const string Scene::effect_fragment_shader_code =
+
+        "#version 330\n"
+        ""
+        "uniform sampler2D sampler2d;"
+        ""
+        "in  vec2 texture_uv;"
+        "out vec4 fragment_color;"
+        ""
+        "void main()"
+        "{"
+        "    vec3 color = texture (sampler2d, texture_uv.st).rgb;"
+        "    float i = (color.r + color.g + color.b) * 0.3333333333;"
+        "    fragment_color = vec4(vec3(i, i, i) * vec3(1.0, 0.75, 0.5), 1.0);"
+        "}";
+
     const string Scene::texture_path = "../assets/Stone_Base_Color.png";
 
     Scene::Scene(unsigned width, unsigned height)
@@ -89,39 +120,54 @@ namespace udit
         angle(0)
         //terrain(10.f, 10.f, 50, 50)
     {
+        /// Postprocesado
+        // Se crea la textura y se dibuja algo en ella:
+        build_framebuffer();
+
         // Se compilan y se activan los shaders:
-        GLuint program_id = compile_shaders ();
+        program_id = compile_shaders(vertex_shader_code, fragment_shader_code);
+        effect_program_id = compile_shaders(effect_vertex_shader_code, effect_fragment_shader_code);
 
-        glUseProgram (program_id);
+        glUseProgram(program_id);
 
-        model_view_matrix_id = glGetUniformLocation (program_id, "model_view_matrix");
-        projection_matrix_id = glGetUniformLocation (program_id, "projection_matrix");
-            normal_matrix_id = glGetUniformLocation (program_id, "normal_matrix"    );
+        model_view_matrix_id = glGetUniformLocation(program_id, "model_view_matrix");
+        projection_matrix_id = glGetUniformLocation(program_id, "projection_matrix");
+        normal_matrix_id = glGetUniformLocation(program_id, "normal_matrix");
 
-        //// Se carga la textura y se envía a la GPU:
+        // Se carga la textura y se envía a la GPU:
         texture_id = create_texture_2d(texture_path);
-
         there_is_texture = texture_id > 0;
 
         // Se establece la altura máxima del height map en el vertex shader:
-        glUniform1f(glGetUniformLocation(program_id, "max_height"), 5.f);
-        
-        configure_material (program_id);
-        configure_light    (program_id);
+        //glUniform1f(glGetUniformLocation(program_id, "max_height"), 5.f);
+
+        configure_material(program_id);
+        configure_light(program_id);
 
         // Se establece la configuración básica:
         glEnable(GL_CULL_FACE);
-        glEnable(GL_DEPTH_TEST);
-        glClearColor(.1f, .1f, .1f, 1.f);
+        // glEnable(GL_DEPTH_TEST);     // PANTALLAZO NEGRO CON ESTO ACTIVADO!!!
+        glClearColor(0.f, 0.f, 0.f, 1.f);
 
-        resize (width, height);
+        resize(width, height);
 
         load_mesh("../assets/Terreno.obj");
     }
+
     Scene::~Scene()
     {
         glDeleteVertexArrays(1, &vao_id);
         glDeleteBuffers(VBO_COUNT, vbo_ids);
+
+        glDeleteVertexArrays(1, &framebuffer_quad_vao);
+        glDeleteBuffers(2, framebuffer_quad_vbos);
+
+        glDeleteProgram(program_id);
+
+        if (there_is_texture)
+        {
+            glDeleteTextures(1, &texture_id);
+        }
     }
 
     void Scene::update ()
@@ -131,6 +177,11 @@ namespace udit
 
     void Scene::render()
     {
+        glViewport(0, 0, framebuffer_width, framebuffer_height);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_id);         // Se activa el framebuffer de la textura
+        glUseProgram(program_id);
+
+        glClearColor(.8f, .8f, .8f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Se selecciona la textura si está disponible:
@@ -159,16 +210,15 @@ namespace udit
 
         // Texturizado
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture_id);
-        glUniform1i(glGetUniformLocation(program_id, "sampler"), 0);
+        glBindTexture(GL_TEXTURE_2D, out_texture_id);
+        glUniform1i(glGetUniformLocation(effect_program_id, "sampler2d"), 0);
 
         // Se dibuja la malla:
         glBindVertexArray(vao_id);
         glDrawElements(GL_TRIANGLES, number_of_indices, GL_UNSIGNED_SHORT, 0);
 
+        //// Se renderiza el cubo en el framebuffer:
         //cube.render();
-        // Se renderiza el terreno
-        //terrain.render();
 
         /// SEGUNDA ETAPA (RENDER DE LOS OBJETOS TRANSPARENTES):
         // Se habilita la mezcla con el color de fondo usando el canal alpha y se deshabilita la escritura en el Z-Buffer:
@@ -190,7 +240,15 @@ namespace udit
         // Se deshabilita la mezcla con el fondo y se restaura escritura en el Z-Buffer:
         glDisable(GL_BLEND);
         glDepthMask(GL_TRUE);
+
+        /// Postprocesado
+        // Renderizado postprocesado final a pantalla
+        //glBindFramebuffer(GL_FRAMEBUFFER, 0);                // Volver a la ventana
+        //glViewport(0, 0, window_width, window_height);       // Ajuste de viewport
+
+        render_framebuffer();                                // Dibuja el framebuffer en pantalla
     }
+
 
     /// <summary>
     ///  OpenGL adapta el campo visual horizontal/vertical según la nueva forma de la ventana si se cambia su tamaño
@@ -199,12 +257,135 @@ namespace udit
     /// <param name="height"></param>
     void Scene::resize (unsigned width, unsigned height)
     {
+        window_width  = width;
+        window_height = height;
+
         glm::mat4 projection_matrix = glm::perspective (20.f, GLfloat(width) / height, 1.f, 5000.f);
 
         glUniformMatrix4fv (projection_matrix_id, 1, GL_FALSE, glm::value_ptr(projection_matrix));
 
         glViewport (0, 0, width, height);
     }
+
+    /// ------------------ POSTPROCESADO ------------------
+
+    void Scene::build_framebuffer()
+    {
+        // Se crea un framebuffer en el que poder renderizar:
+        {
+            glGenFramebuffers(1, &framebuffer_id);
+            glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_id);
+        }
+
+        // Se crea una textura que será el búffer de color vinculado al framebuffer:
+        {
+            glGenTextures(1, &out_texture_id);
+            glBindTexture(GL_TEXTURE_2D, out_texture_id);
+
+            // El búfer de color tendrá formato RGB:
+
+            glTexImage2D
+            (
+                GL_TEXTURE_2D,
+                0,
+                GL_RGB,
+                framebuffer_width,
+                framebuffer_height,
+                0,
+                GL_RGB,
+                GL_UNSIGNED_BYTE,
+                0
+            );
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        }
+
+        // Se crea un Z-Buffer para usarlo en combinación con el framebuffer:
+        {
+            glGenRenderbuffers(1, &depthbuffer_id);
+            glBindRenderbuffer(GL_RENDERBUFFER, depthbuffer_id);
+            glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, framebuffer_width, framebuffer_height);
+            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthbuffer_id);
+        }
+
+        // Se configura el framebuffer:
+        {
+            glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, out_texture_id, 0);
+
+            const GLenum draw_buffer = GL_COLOR_ATTACHMENT0;
+
+            glDrawBuffers(1, &draw_buffer);
+        }
+
+        // Se comprueba que el framebuffer está listo:
+
+        assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
+
+        // Se desvincula el framebuffer:
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        // Se crea la malla del quad necesario para hacer el render del framebuffer:
+
+        static const GLfloat quad_positions[] =
+        {
+            +1.0f, -1.0f, 0.0f,
+            +1.0f, +1.0f, 0.0f,
+            -1.0f, +1.0f, 0.0f,
+            -1.0f, +1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f,
+            +1.0f, -1.0f, 0.0f,
+        };
+
+        static const GLfloat quad_texture_uvs[] =
+        {
+            +1.0f,  0.0f,
+            +1.0f, +1.0f,
+             0.0f, +1.0f,
+             0.0f, +1.0f,
+             0.0f,  0.0f,
+             1.0f,  0.0f,
+        };
+
+        glGenVertexArrays(1, &framebuffer_quad_vao);
+        glGenBuffers(2, framebuffer_quad_vbos);
+
+        glBindVertexArray(framebuffer_quad_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, framebuffer_quad_vbos[0]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quad_positions), quad_positions, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, framebuffer_quad_vbos[1]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quad_texture_uvs), quad_texture_uvs, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    }
+
+    void Scene::render_framebuffer()
+    {
+        glViewport(0, 0, window_width, window_height);
+
+        // Se activa el framebuffer de la ventana:
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        glUseProgram(effect_program_id);
+
+        // Se activa la textura del framebuffer y se renderiza en la ventana:
+
+        glBindTexture(GL_TEXTURE_2D, out_texture_id);
+
+        glBindVertexArray(framebuffer_quad_vao);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+
+    ///----------------------------------------------------
 
     GLuint Scene::compile_shaders ()
     {
@@ -249,6 +430,64 @@ namespace udit
         if (!succeeded) show_linkage_error(program_id);
 
         // Se liberan los shaders compilados una vez se han linkado:
+        glDeleteShader(vertex_shader_id);
+        glDeleteShader(fragment_shader_id);
+
+        return (program_id);
+    }
+
+    GLuint Scene::compile_shaders(const std::string& vertex_shader_code, const std::string& fragment_shader_code)
+    {
+        GLint succeeded = GL_FALSE;
+
+        // Se crean objetos para los shaders:
+
+        GLuint   vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+        GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+        // Se carga el código de los shaders:
+
+        const char* vertex_shaders_code[] = { vertex_shader_code.c_str() };
+        const char* fragment_shaders_code[] = { fragment_shader_code.c_str() };
+        const GLint    vertex_shaders_size[] = { GLint(vertex_shader_code.size()) };
+        const GLint  fragment_shaders_size[] = { GLint(fragment_shader_code.size()) };
+
+        glShaderSource(vertex_shader_id, 1, vertex_shaders_code, vertex_shaders_size);
+        glShaderSource(fragment_shader_id, 1, fragment_shaders_code, fragment_shaders_size);
+
+        // Se compilan los shaders:
+
+        glCompileShader(vertex_shader_id);
+        glCompileShader(fragment_shader_id);
+
+        // Se comprueba que si la compilación ha tenido éxito:
+
+        glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &succeeded);
+        if (!succeeded) show_compilation_error(vertex_shader_id);
+
+        glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &succeeded);
+        if (!succeeded) show_compilation_error(fragment_shader_id);
+
+        // Se crea un objeto para un programa:
+
+        GLuint program_id = glCreateProgram();
+
+        // Se cargan los shaders compilados en el programa:
+
+        glAttachShader(program_id, vertex_shader_id);
+        glAttachShader(program_id, fragment_shader_id);
+
+        // Se linkan los shaders:
+
+        glLinkProgram(program_id);
+
+        // Se comprueba si el linkage ha tenido éxito:
+
+        glGetProgramiv(program_id, GL_LINK_STATUS, &succeeded);
+        if (!succeeded) show_linkage_error(program_id);
+
+        // Se liberan los shaders compilados una vez se han linkado:
+
         glDeleteShader(vertex_shader_id);
         glDeleteShader(fragment_shader_id);
 
@@ -363,6 +602,8 @@ namespace udit
         glUniform1f(diffuse_intensity, 0.8f                  );
     }
 
+    /// ------------------ TEXTURIZADO ------------------ 
+
     GLuint Scene::create_texture_2d(const std::string& texture_path)
     {
         auto image = load_image(texture_path);
@@ -383,7 +624,7 @@ namespace udit
             // Se habilitan las texturas, se genera un id para un búfer de textura,
             // se selecciona el búfer de textura creado y se configuran algunos de
             // sus parámetros:
-            GLuint texture_id;
+            //GLuint texture_id;
 
             glEnable(GL_TEXTURE_2D);
             glGenTextures(1, &texture_id);
@@ -453,8 +694,9 @@ namespace udit
         return nullptr;
     }
 
-
-    /// ------------------ Error utilities ---------------------------------
+    /// -------------------------------------------------
+    
+    /// ------------------ ERRORES (Utilidades) -----------------
 
     void Scene::show_compilation_error (GLuint shader_id)
     {
@@ -496,7 +738,7 @@ namespace udit
         assert(false);
     }
 
-    /// ---------------------------------------------------
+    /// ---------------------------------------------------------
 
     glm::vec3 Scene::random_color()
     {
--- code/Scene.hpp
+++ code/Scene.hpp
@@ -32,42 +32,61 @@ namespace udit
             VBO_COUNT
         };
 
-        static const std::string    vertex_shader_code;
-        static const std::string    fragment_shader_code;
-        static const std::string    texture_path;
+        static const GLsizei  framebuffer_width = 256;
+        static const GLsizei framebuffer_height = 256;
 
+        static const std::string          vertex_shader_code;
+        static const std::string        fragment_shader_code;
+        static const std::string                texture_path;
+        static const std::string   effect_vertex_shader_code;
+        static const std::string effect_fragment_shader_code;
 
         GLint   model_view_matrix_id;
         GLint   projection_matrix_id;
         GLint       normal_matrix_id;
+        GLuint        framebuffer_id;
+        GLuint        depthbuffer_id;
+        GLuint        out_texture_id;
 
+        /// Terreno (Mallas de elevación)
         //Terrain terrain;
 
-        Cube    cube;
-        float   angle;
+        Cube  cube;
 
-        // Cargar modelos 3D
+        float angle;
+
+        /// Cargar modelos 3D
         GLuint  vao_model, vbo_model, ebo_model;
         GLsizei index_count = 0;
 
-        // Color aleatorio
+        /// Color aleatorio
         GLuint  vbo_ids[VBO_COUNT];
-        GLuint  vao_id;
-        GLsizei number_of_indices;
+        GLuint              vao_id;
+        GLsizei  number_of_indices;
 
-        // Cargar texturas
-        GLuint program_id;
-        GLuint texture_id = 0;
+        /// Cargar texturas
+        GLuint          program_id;
+        GLuint      texture_id = 0;
         GLuint use_vertex_color_id;
-        bool   there_is_texture;
+        //GLuint     cube_program_id;
+        GLuint   effect_program_id;
+
+        bool      there_is_texture;
+
+        /// Postprocesado
+        GLuint     framebuffer_quad_vao;
+        GLuint framebuffer_quad_vbos[2];
+
+        int window_width;
+        int window_height;
 
     public:
 
-        // Cámara
+        /// Cámara
         Camera camera;
 
-        Scene(unsigned width, unsigned height);
-       ~Scene();
+        Scene (unsigned width, unsigned height);
+       ~Scene ();
 
         void   update       ();
         void   render       ();
@@ -76,14 +95,19 @@ namespace udit
 
     private:
 
-        GLuint compile_shaders        ();
-        void   show_compilation_error (GLuint  shader_id);
-        void   show_linkage_error     (GLuint program_id);
-        void   load_mesh(const std::string& mesh_file_path);
-        glm::vec3   random_color();
+        /// Postprocesado
+        void   build_framebuffer();
+        void   render_framebuffer();
 
-        void   configure_material(GLuint program_id);
-        void   configure_light(GLuint program_id);
+        GLuint      compile_shaders        ();
+        GLuint      compile_shaders        (const std::string& vertex_shader_code, const std::string& fragment_shader_code);
+        void        show_compilation_error (GLuint  shader_id);
+        void        show_linkage_error     (GLuint program_id);
+        void        load_mesh              (const std::string& mesh_file_path);
+        glm::vec3   random_color           ();
+
+        void   configure_material (GLuint program_id);
+        void   configure_light    (GLuint program_id);
 
         GLuint create_texture_2d(const std::string& texture_path);
         std::unique_ptr< Color_Buffer > load_image(const std::string& image_path);
